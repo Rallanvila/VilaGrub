0848ab14d4295f2b9ca54735278478baab610a33
Allan <74523880+Rallanvila@users.noreply.github.com>
2021-10-05 00:50:53 -0400

    add image to map

--- components/DuoCard.js
+++ components/DuoCard.js
@@ -7,37 +7,36 @@ import duoCardContent from "../data/content-DuoCard";
 
 function DuoCard() {
 	return (
-		<>
-			{duoCardContent.map((c) => {
-				<FlexCenter key={c.id} bg={c.background}>
-					<h2>{c.image}</h2>
-					<h2>{c.title}</h2>
-					<p>{c.description}</p>
-					<Button border={c.btnBorder}>Order Now</Button>
-				</FlexCenter>;
-			})}
-		</>
+		// <>
+		// 	{duoCardContent.map((c) => {
+		// 		<FlexCenter key={c.id} bg={c.background}>
+		// 			<h2>{c.image}</h2>
+		// 			<h2>{c.title}</h2>
+		// 			<p>{c.description}</p>
+		// 			<Button border={c.btnBorder}>Order Now</Button>
+		// 		</FlexCenter>;
+		// 	})}
+		// </>
+		duoCardContent.map((m) => (
+			<>
+				<FlexCenter>
+					<h2>{m.image}</h2>
+					<h2>{m.title}</h2>
+					<p>{m.description}</p>
+					<Button border={m.btnBorder}>Order Now</Button>
+				</FlexCenter>
+			</>
+		))
 
-		// <Grid>
-		// 	<FlexCenter>
-		// 		<h2>Img</h2>
-		// 		<h2>Order Online</h2>
-		// 		<p>
-		// 			Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati
-		// 			fuga natus repudiandae voluptate beatae aperiam?
-		// 		</p>
-		// 		<Button border="#212529">Order Now</Button>
-		// 	</FlexCenter>
-		// 	<FlexCenter>
-		// 		<h2>Img</h2>
-		// 		<h2>Order Online</h2>
-		// 		<p>
-		// 			Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati
-		// 			fuga natus repudiandae voluptate beatae aperiam?
-		// 		</p>
-		// 		<Button border="#212529">Order Now</Button>
-		// 	</FlexCenter>
-		// </Grid>
+		// <FlexCenter>
+		// 	<h2>image</h2>
+		// 	<h2>Order Online</h2>
+		// 	<p>
+		// 		Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati fuga
+		// 		natus repudiandae voluptate beatae aperiam?
+		// 	</p>
+		// 	<Button border="#212529">Order Now</Button>
+		// </FlexCenter>
 	);
 }
 
