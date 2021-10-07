da6022a812ada40ea07135fdf97a092018b0ecd4
Allan <74523880+Rallanvila@users.noreply.github.com>
2021-10-07 18:45:05 -0400

    fixed nav

---
+++ components/SignInHeader/StyledSignInHeader.styled.js
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const StyledSignInForm = styled.form`
+	display: flex;
+	align-items: center;
+	.card-content {
+		display: flex;
+		flex-direction: column;
+		padding: 3rem 2rem 10rem;
+		width: 100%;
+		.text-field {
+			width: 100%;
+			margin-bottom: 2rem;
+		}
+		.forgot {
+			font-weight: 700;
+			text-decoration: underline;
+			margin-bottom: 1rem;
+			cursor: pointer;
+			color: #1976d2;
+		}
+	}
+`;
