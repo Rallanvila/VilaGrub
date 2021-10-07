87ed985819e14c57df3dc21577828d996a4bf669
Allan <74523880+Rallanvila@users.noreply.github.com>
2021-10-07 18:39:24 -0400

    signin form created

---
+++ helpers/SignInNav.styled.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const SignInNav = styled.nav`
+	width: 100%;
+	padding: 1rem 1rem;
+	-webkit-box-shadow: -3px 5px 7px 0px rgba(0, 0, 0, 0.52);
+	box-shadow: -2px 2px 7px 0px rgba(0, 0, 0, 0.32);
+	width: fit-content;
+`;
