e69a3727e154181f19d0bc0f3848ccb080e4ed11
Allan <74523880+Rallanvila@users.noreply.github.com>
2021-10-08 17:21:19 -0400

    setup grid for item details

---
+++ components/MenuItemDetailsDetails/MenuItemDetailsDetails.js
@@ -0,0 +1,27 @@
+import { Grid } from "../../helpers/Grid.styled";
+
+export function MenuItemDetailsDetails() {
+	return (
+		<Grid
+			style={{ padding: "0 3rem", textAlign: "start", alignItems: "start" }}
+		>
+			<div>
+				<p>
+					Espresso shots topped with hot water create a light layer of crema
+					culminating in this wonderfully rich cup with depth and nuance. Pro
+					Tip: For an additional boost, ask your barista to try this with an
+					extra shot.
+				</p>
+			</div>
+			<div>
+				<h2>Nutritional Information</h2>
+				<p>
+					Espresso shots topped with hot water create a light layer of crema
+					culminating in this wonderfully rich cup with depth and nuance. Pro
+					Tip: For an additional boost, ask your barista to try this with an
+					extra shot.
+				</p>
+			</div>
+		</Grid>
+	);
+}
