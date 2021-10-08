9614300567b665414f3c3a900ed33613e8435f64
Allan <74523880+Rallanvila@users.noreply.github.com>
2021-10-08 12:11:20 -0400

    added netlify dependency

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,20 @@
 import Head from "next/head";
 import "normalize.css";
-import styles from "../styles/Home.module.scss";
 import Header from "../components/header/Header";
 import MainBanner from "../components/hero/MainBanner";
 import { CardBanner } from "../components/main-cards/CardBanner";
 import DuoCard from "../components/duo-cards/DuoCard";
 import Footer from "../components/footer/Footer";
 
-export const theme = {
-	colors: {
-		brandBlue: "#294442",
-		brandBrown: "#c69250",
-		brandGreen: "#89a525",
-		brandGrey: "#789494",
-		dark: "#212529",
-	},
-};
+// export const theme = {
+// 	colors: {
+// 		brandBlue: "#294442",
+// 		brandBrown: "#c69250",
+// 		brandGreen: "#89a525",
+// 		brandGrey: "#789494",
+// 		dark: "#212529",
+// 	},
+// };
 
 export default function Home() {
 	return (
